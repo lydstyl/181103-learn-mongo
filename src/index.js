558c274c01331475bb83f743f66d5eaa47321d70
Gabriel Brun <lydstyl@gmail.com>
2018-11-04 15:11:15 +0100

    tuto 20min

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // https://www.youtube.com/watch?v=o3ka5fYysBM&index=7&t=0s&list=PLo2PSuBYZiOodPcZEAK3W7G0aZHRSqfO8
 // 18min / 1h39
 
-console.log('hello world');
+let express = resquire('express')
+
+let app = express()
