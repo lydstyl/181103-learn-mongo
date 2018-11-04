268029ebc30d899d3fe2b53b4afa1205d2ad1711
Gabriel Brun <lydstyl@gmail.com>
2018-11-04 15:47:51 +0100

    42min query req.query.xxx and req.params.xxx with insomnia

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 // https://www.youtube.com/watch?v=o3ka5fYysBM&index=7&t=0s&list=PLo2PSuBYZiOodPcZEAK3W7G0aZHRSqfO8
+// installer insomnia ou postman
+
 // 18min / 1h39
 
-let express = resquire('express')
+let express = require('express')
 
 let app = express()
+
+let personRoute = require('./routes/person')
+
+app.use(personRoute)
+app.use(express.static( 'public' ))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server has started on ${PORT}`))
